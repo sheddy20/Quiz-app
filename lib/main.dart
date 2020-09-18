8eb6f81b0cfb1568e0848df23357ae6b2d002692
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-18 20:27:05 +0100

    done

--- lib/main.dart
+++ lib/main.dart
@@ -13,38 +13,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> questions = [
+    'what\'s your favourite car',
+    'what\'s your name',
+    'what\'s your girlfriend name',
+  ];
+
+  void _answerQuestion() {
+    print('Answer Chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Flutter Text Widget'),
-            Card(
-              child: Text('Flutter Card Widget'),
-              margin: EdgeInsets.all(16.0),
-              color: Colors.blue,
-              shadowColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0)),
-            ),
-            MaterialButton(
-              child: Text('Material Button Widget'),
-              onPressed: () {},
-              color: Colors.blue,
-              textColor: Colors.white,
-              splashColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Text('The Questions'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: _answerQuestion,
+            textColor: Colors.white,
+            color: Colors.black,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: _answerQuestion,
+            textColor: Colors.white,
+            color: Colors.black,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: _answerQuestion,
+            textColor: Colors.white,
+            color: Colors.black,
+          ),
+        ],
       ),
     );
   }
